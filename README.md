dd8113005e6f0a25588c459d4bddf1e23698eee1
Raito-95 <info.raito.site@gmail.com>
2024-07-30 19:38:27 +0800

    Refactor message handler

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
-# Slack Bot Project
+# Llama Bot Project
 
-This project is a modular Slack Bot built using Python and the Slack Bolt framework. The bot integrates with the Llama and Llava chat models to handle text and image messages, update user conversations, and provide loading animations.
+This project is a modular bot built using Python that integrates with LINE, Slack, and a generic interface. The bot leverages Llama and Llava chat models to handle text and image messages.
 
 ## Project Structure
 
 ```
 project/
-|-- slack_bot.py
+|-- llama_bot.py
 |-- app/
 |   |-- __init__.py
 |   |-- event_handlers.py
@@ -29,8 +29,6 @@ project/
 ```
 
 ## Requirements
-### Slack bolt for python
-Please refer to Slack website to set up your bot : https://slack.dev/bolt-python/tutorial/getting-started
 
 ### Python Packages
 
@@ -58,38 +56,63 @@ LINE_CHANNEL_ACCESS_TOKEN=your-line-channel-access-token
 LINE_CHANNEL_SECRET=your-line-channel-secret
 SLACK_BOT_TOKEN=your-slack-bot-token
 SLACK_SIGNING_SECRET=your-slack-signing-secret
-SLACK_APP_TOKEN=your-slack-app-token 
-LLAMA_API_URL = your-llama-api-url
-LLAVA_API_URL = your-llava-api-url
+SLACK_APP_TOKEN=your-slack-app-token
+LLAMA_API_URL=your-llama-api-url
+LLAVA_API_URL=your-llava-api-url
 ```
 
 ## How to Run
 
-1. **Clone the repository**:
-    ```bash
-    git clone https://github.com/kzger/Slack_bolt.git
-    cd slack-bot-project
-    ```
+### 1. Clone the repository:
 
-2. **Set up your environment**:
-    - Create a virtual environment:
-      ```bash
-      python -m venv venv
-      source venv/bin/activate   # On Windows use `venv\Scripts\activate`
-      ```
-    - Install the required packages:
-      ```bash
-      pip install -r requirements.txt
-      ```
+```bash
+git clone https://github.com/kzger/Slack_bolt.git
+cd Slack_bolt
+```
 
-3. **Configure environment variables**:
-    - Create a `.env` file in the project root directory with the necessary environment variables as mentioned above.
+### 2. Set up your environment:
 
-4. **Run the application**:
-    ```bash
-    python slack_bot.py
-    ```
+Create a virtual environment:
 
+```bash
+python -m venv venv
+source venv/bin/activate   # On Windows use `venv\Scripts\activate`
+```
+
+Install the required packages:
+
+```bash
+pip install -r requirements.txt
+```
+
+### 3. Configure environment variables:
+
+Create a `.env` file in the project root directory with the necessary environment variables as mentioned above.
+
+### 4. Run the application:
+
+```bash
+python llama_bot.py
+```
+
+## Integrating with LINE
+
+To set up your bot with LINE, follow these steps:
+
+1. Go to the LINE Developers Console.
+2. Create a new provider and channel.
+3. Note down your `LINE_CHANNEL_ACCESS_TOKEN` and `LINE_CHANNEL_SECRET`.
+4. Add the callback endpoint in your LINE channel settings for the webhook URL.
+
+## Integrating with Slack
+
+### Slack Setup
+
+Please refer to the Slack website to set up your bot: [Slack Bolt for Python](https://slack.dev/bolt-python/tutorial/getting-started)
+
+1. Go to the [Slack API](https://api.slack.com/) and create a new app.
+2. Enable the necessary permissions and event subscriptions.
+3. Note down your `SLACK_BOT_TOKEN`, `SLACK_SIGNING_SECRET`, and `SLACK_APP_TOKEN`.
 
 ## License
 
--- app/message_handler.py
+++ app/message_handler.py
@@ -1,103 +1,226 @@
+import logging
 from collections import defaultdict, deque
 from typing import Dict, Deque, List, Any
 from services.llama_service import request_llama_chat
 from services.llava_service import request_llava_chat
 from services.prompt_service import request_prompt_chat
 from app.utils import is_image_file, download_image, image_to_base64
-from config.settings import LLAMA_API_URL, LLAVA_API_URL, SLACK_BOT_TOKEN
+from config.settings import LLAMA_API_URL, LLAVA_API_URL, SLACK_BOT_TOKEN, LINE_CHANNEL_ACCESS_TOKEN
 from model.model_list import LLModelList
+import requests
+from threading import Lock
+from linebot.v3.messaging import Configuration, ApiClient, MessagingApi
+from linebot.v3.messaging.models import ReplyMessageRequest, TextMessage, ShowLoadingAnimationRequest
 
-# Initialize data structures
+# Set up logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 user_conversations: Dict[str, Deque[Dict[str, str]]] = defaultdict(lambda: deque(maxlen=10))
 user_system_prompts: Dict[str, Deque[str]] = defaultdict(lambda: deque(maxlen=1))
+user_active_request: Dict[str, bool] = defaultdict(bool)
+user_locks: Dict[str, Lock] = defaultdict(Lock)
 
+configuration = Configuration(access_token=LINE_CHANNEL_ACCESS_TOKEN)
 
-# Initialize models
 def initialize_models():
-    print("Initializing models...")
-    request_llama_chat(LLAMA_API_URL, LLModelList.ZH_TW_L3_8B, [{"role": "system", "content": "Initialize ZH_TW_L3_8B"}],)
-    request_llama_chat(LLAMA_API_URL, LLModelList.LLAMA3_8B, [{"role": "system", "content": "Initialize LLAMA3_8B"}],)
-    request_llava_chat(LLAVA_API_URL, LLModelList.LLAVA, "Initialize LLAVA", [])
-    request_prompt_chat(LLAMA_API_URL, LLModelList.PROMPT, [{"role": "system", "content": "Initialize PROMPT"}],)
+    logger.info("Initializing models...")
+    request_llama_chat(LLAMA_API_URL, LLModelList.ZH_TW_L3_8B, [{"role": "system", "content": "Initialize ZH_TW_L3_8B"}])
+    # request_llama_chat(LLAMA_API_URL, LLModelList.LLAMA3_8B, [{"role": "system", "content": "Initialize LLAMA3_8B"}],)
+    # request_llava_chat(LLAVA_API_URL, LLModelList.LLAVA, "Initialize LLAVA", [])
+    # request_prompt_chat(LLAMA_API_URL, LLModelList.PROMPT, [{"role": "system", "content": "Initialize PROMPT"}],)
 
+def handle_message(handler) -> Any:
+    user_id = handler.get_user_id()
+    with user_locks[user_id]:
+        if user_active_request[user_id]:
+            return
+        user_active_request[user_id] = True
 
-def handle_message(event: Dict[str, Any]) -> str:
-    """Handle message based on event type."""
-    text: str = str(event.get("text", ""))
-    if text.startswith("!") or text.startswith("_") or text.startswith("?"):
-        return handle_special_commands(event)
-    elif "files" in event:
-        return handle_image_message(event)
-    elif "sdxl" in event:
-        return handle_prompt_message(event)
-    else:
-        return handle_text_message(event)
+    try:
+        return handler.process_message()
+    finally:
+        with user_locks[user_id]:
+            user_active_request[user_id] = False
 
+def show_loading_animation(chat_id: str, loading_seconds: int):
+    url = "https://api.line.me/v2/bot/chat/loading/start"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {LINE_CHANNEL_ACCESS_TOKEN}"
+    }
+    data = ShowLoadingAnimationRequest(chatId=chat_id, loadingSeconds=loading_seconds).to_json()
+    response = requests.post(url, headers=headers, data=data)
+    return response.json()
 
-def handle_special_commands(event: Dict[str, Any]) -> str:
-    user_id: str = str(event.get("user"))
-    text: str = str(event.get("text", ""))
+class GenericHandler:
+    def __init__(self, event: Dict[str, Any]):
+        self.event = event
 
-    if text.startswith("!"):
-        user_system_prompts[user_id].append(text[1:])
-        return f"System prompt updated: {text[1:]}"
-    elif text.startswith("_"):
-        user_conversations[user_id].clear()
-        user_system_prompts[user_id].clear()
-        return "All records cleared."
-    elif text.startswith("?"):
-        return handle_question_message(user_id, text)
-    else:
-        return "Unknown command."
+    def get_user_id(self) -> str:
+        return str(self.event.get("user"))
 
+    def process_message(self) -> str:
+        if "sdxl" in self.event:
+            return self.handle_prompt_message()
+        return self.handle_text_en_message()
 
-def handle_question_message(user_id: str, text: str) -> str:
-    """Handle messages starting with '?'."""
-    user_conversations[user_id].append({"role": "user", "content": text})
-    messages: List[Dict[str, str]] = [{"role": "system", "content": prompt} for prompt in user_system_prompts[user_id]]
-    messages += list(user_conversations[user_id])
+    def handle_special_commands(self) -> str:
+        user_id: str = self.get_user_id()
+        text: str = str(self.event.get("text"))
+        if text.startswith("!"):
+            user_system_prompts[user_id].append(text[1:])
+            return f"System prompt updated: {text[1:]}"
+        elif text.startswith("_"):
+            user_conversations[user_id].clear()
+            user_system_prompts[user_id].clear()
+            return "All records cleared."
+        else:
+            return "Unknown command."
 
-    response: str = request_llama_chat(LLAMA_API_URL, LLModelList.ZH_TW_L3_8B, messages)
-    if response:
-        user_conversations[user_id].append({"role": "assistant", "content": response})
-    return response
+    def handle_prompt_message(self) -> str:
+        user_id: str = self.get_user_id()
+        sdxl: str = str(self.event.get("sdxl"))
+        user_conversations[user_id].append({"role": "user", "content": sdxl})
+        messages: List[Dict[str, str]] = [{"role": "system", "content": prompt} for prompt in user_system_prompts[user_id]]
+        messages += list(user_conversations[user_id])
+        response: str = request_prompt_chat(LLAMA_API_URL, LLModelList.PROMPT, messages)
+        if response:
+            user_conversations[user_id].append({"role": "assistant", "content": response})
+        return response
 
+    def handle_text_en_message(self) -> str:
+        user_id: str = self.get_user_id()
+        text: str = str(self.event.get("text"))
+        user_conversations[user_id].append({"role": "user", "content": text})
+        messages: List[Dict[str, str]] = [{"role": "system", "content": prompt} for prompt in user_system_prompts[user_id]]
+        messages += list(user_conversations[user_id])
+        response: str = request_llama_chat(LLAMA_API_URL, LLModelList.LLAMA3_8B, messages)
+        if response:
+            user_conversations[user_id].append({"role": "assistant", "content": response})
+        return response
 
-def handle_image_message(event: Dict[str, Any]) -> str:
-    text: str = str(event.get("text", ""))
-    response: str = ""
-    for file in event.get("files", []):
-        if is_image_file(file):
-            image_content = download_image(file["url_private"], SLACK_BOT_TOKEN)
-            if image_content:
-                base64_image: str = image_to_base64(image_content)
-                response += request_llava_chat(LLAVA_API_URL, LLModelList.LLAVA, text, [base64_image])
-    return response
+    def reply_message(self, response_text: str) -> Any:
+        raise NotImplementedError
 
+class SlackHandler(GenericHandler):
+    def __init__(self, event: Dict[str, Any]):
+        super().__init__(event)
 
-def handle_text_message(event: Dict[str, Any]) -> str:
-    user_id: str = str(event.get("user"))
-    text: str = str(event.get("text", ""))
-    user_conversations[user_id].append({"role": "user", "content": text})
-    messages: List[Dict[str, str]] = [{"role": "system", "content": prompt} for prompt in user_system_prompts[user_id]]
-    messages += list(user_conversations[user_id])
+    def process_message(self) -> str:
+        if "files" in self.event:
+            return self.handle_image_message()
+        return self.handle_text_en_message()
 
-    response: str = request_llama_chat(LLAMA_API_URL, LLModelList.LLAMA3_8B, messages)
-    if response:
-        user_conversations[user_id].append({"role": "assistant", "content": response})
-    return response
+    def handle_image_message(self) -> str:
+        text: str = str(self.event.get("text", ""))
+        response: str = ""
+        for file in self.event.get("files", []):
+            if is_image_file(file):
+                image_content = download_image(file["url_private"], SLACK_BOT_TOKEN)
+                if image_content:
+                    base64_image: str = image_to_base64(image_content)
+                    response += request_llava_chat(LLAVA_API_URL, LLModelList.LLAVA, text, [base64_image])
+        return response
 
+class LineHandler(GenericHandler):
+    def __init__(self, event: Dict[str, Any]):
+        super().__init__(event)
+        self.user_id = event["source"]["userId"]
+        self.message_type = event["message"]["type"]
+        self.text = event["message"].get("text", "")
+        self.source_type = event["source"]["type"]
+        self.reply_token = event["replyToken"]
+        self.quote_token = event["message"].get("quoteToken", "")
 
-def handle_prompt_message(event: Dict[str, Any]) -> str:
-    sdxl_event_data = event.get("sdxl")
-    if not sdxl_event_data:
-        return "SDXL event data is missing."
+    def get_user_id(self) -> str:
+        return self.user_id
 
-    user_id = str(event.get("user", ""))
-    messages: List[Dict[str, str]] = [{"role": "system", "content": prompt} for prompt in user_system_prompts[user_id]]
-    messages.append({"role": "user", "content": sdxl_event_data})
+    def process_message(self) -> str:
+        if self.source_type == "user":
+            self.show_loading_animation(self.user_id, 60)
+            if self.text.startswith("!") or self.text.startswith("_"):
+                response = self.handle_special_commands()
+            else:
+                if self.message_type == "image":
+                    response = self.handle_image_message()
+                else:
+                    response = self.handle_text_tw_message()
+        elif self.source_type in ["group", "room"]:
+            if not (self.text.startswith("!") or self.text.startswith("_") or self.text.startswith("?")):
+                return "Ignored non-special command message in group/room."
+            response = self.handle_special_commands()
+        else:
+            response = self.handle_text_tw_message()
 
-    response: str = request_prompt_chat(LLAMA_API_URL, LLModelList.PROMPT, messages)
-    if response:
-        user_conversations[user_id].append({"role": "assistant", "content": response})
-    return response
+        return self.reply_message(response)
+
+    def show_loading_animation(self, chat_id: str, loading_seconds: int):
+        url = "https://api.line.me/v2/bot/chat/loading/start"
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {LINE_CHANNEL_ACCESS_TOKEN}"
+        }
+        data = ShowLoadingAnimationRequest(chatId=chat_id, loadingSeconds=loading_seconds).to_json()
+        response = requests.post(url, headers=headers, data=data)
+        return response.json()
+
+    def handle_special_commands(self) -> str:
+        if self.text.startswith("!"):
+            user_system_prompts[self.user_id].append(self.text[1:])
+            return f"System prompt updated: {self.text[1:]}"
+        elif self.text.startswith("_"):
+            user_conversations[self.user_id].clear()
+            user_system_prompts[self.user_id].clear()
+            return "All records cleared."
+        elif self.text.startswith("?"):
+            self.text = self.text[1:]
+            return self.handle_text_tw_message()
+        else:
+            return "Unknown command."
+
+    def handle_text_tw_message(self) -> str:
+        user_conversations[self.user_id].append({"role": "user", "content": self.text})
+        messages: List[Dict[str, str]] = [{"role": "system", "content": prompt} for prompt in user_system_prompts[self.user_id]]
+        messages += list(user_conversations[self.user_id])
+        response: str = request_llama_chat(LLAMA_API_URL, LLModelList.ZH_TW_L3_8B, messages)
+        if response:
+            user_conversations[self.user_id].append({"role": "assistant", "content": response})
+        return response
+
+    def handle_image_message(self) -> str:
+        message_id: str = self.event["message"]["id"]
+        image_content = download_image(self.get_line_image_url(message_id), LINE_CHANNEL_ACCESS_TOKEN)
+        if not image_content:
+            return "Failed to download image."
+        base64_image: str = image_to_base64(image_content)
+        response = request_llava_chat(LLAVA_API_URL, LLModelList.LLAVA, self.text, [base64_image])
+        return response
+
+    def get_line_image_url(self, message_id: str) -> str:
+        url = f"https://api-data.line.me/v2/bot/message/{message_id}/content"
+        headers = {
+            "Authorization": f"Bearer {LINE_CHANNEL_ACCESS_TOKEN}"
+        }
+        response = requests.get(url, headers=headers, stream=True)
+        if response.status_code == 200:
+            return url
+        return ""
+
+    def reply_message(self, response_text: str) -> Any:
+        reply_message = TextMessage(
+            quickReply=None,
+            sender=None,
+            text=response_text,
+            emojis=None,
+            quoteToken=self.quote_token,
+        )
+        reply_request = ReplyMessageRequest(
+            replyToken=self.reply_token,
+            messages=[reply_message],
+            notificationDisabled=False,
+        )
+
+        with ApiClient(configuration) as api_client:
+            line_bot_api = MessagingApi(api_client)
+            line_bot_api.reply_message_with_http_info(reply_request)
